884226e2bfaeb3d258320392a44686184808b83a
DESKTOP-FB24039\A <xhanwfl@naver.com>
2021-09-02 17:45:04 +0900

    keystore 만들고 relaesekey 발급

--- app/src/main/java/com/jambosoft/bangbang/SplashActivity.kt
+++ app/src/main/java/com/jambosoft/bangbang/SplashActivity.kt
@@ -1,14 +1,13 @@
 package com.jambosoft.bangbang
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
-import android.content.pm.PackageManager
-
-import android.content.pm.PackageInfo
 import android.util.Base64
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -19,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        getDebugHashKey()
+        getHashKey()
 
         val intent = Intent(this, LoginActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
@@ -40,22 +39,23 @@ class SplashActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
-    private fun getDebugHashKey() {
+
+    private fun getHashKey() {
         var packageInfo: PackageInfo? = null
         try {
             packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-            if (packageInfo == null) Log.e("KeyHash", "KeyHash:null") else {
-                for (signature in packageInfo.signatures) {
-                    val md: MessageDigest = MessageDigest.getInstance("SHA")
-                    md.update(signature.toByteArray())
-                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-                }
-            }
-        } catch (e: NoSuchAlgorithmException) {
-            e.printStackTrace()
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
     }
-
 }
